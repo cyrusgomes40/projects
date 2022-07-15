12488d9bcfef1fe618b179fe883739c8805db818
cyrusgomes40 <92752727+cyrusgomes40@users.noreply.github.com>
2022-07-15 17:27:26 -0500

    Add files via upload

---
+++ soduku.py
@@ -0,0 +1,83 @@
+board = [[7,8,0,4,0,0,1,2,0],
+    [6,0,0,0,7,5,0,0,9],
+    [0,0,0,6,0,1,0,7,8],
+    [0,0,7,0,4,0,2,6,0],
+    [0,0,1,0,5,0,9,3,0],
+    [9,0,4,0,6,0,0,0,5],
+    [0,7,0,3,0,0,0,1,2],
+    [1,2,0,0,0,7,4,0,0],
+    [0,4,9,2,0,6,0,0,7]]
+
+
+def solve(board):
+    find = is_empty(board)
+    if not find:
+        return True
+    else:
+        row, col = find
+
+    for i in range(1,10):
+        if valid(board, [row, col], i):
+            board[row][col] = i
+
+            if solve(board):
+                return True
+
+            board[row][col] = 0
+
+    return False
+
+
+def valid(board, pos, num):
+
+# This checks for the given row
+    for i in range(9):
+       if board[pos[0]][i] == num and pos[1] != i:
+           return False
+
+# This checks for the given column
+    for i in range(9):
+        if board[i][pos[1]] == num and pos[0] != i:
+            return False
+
+# This checks for the given grid
+    X = pos[1]//3
+    Y = pos[0]//3
+    for i in range(Y*3,(Y*3)+3):
+        for j in range(X*3,(X*3)+3):
+            if board[i][j] == num and [i, j] != pos:
+                return False
+
+    return True
+
+# this function is to print the board in a 9x9 grid
+
+def print_board(board):
+    for i in range(len(board)):
+        if i%3 ==0 and i != 0:
+            print("-----------------------")
+
+        for j in range(9):
+            if j % 3 == 0 and j != 0:
+
+                print(" | ", end="")
+
+            if j == 8:
+                print(board[i][j])
+            else:
+                print(str(board[i][j]), end=" ")
+
+
+
+def is_empty(board):
+    for i in range (len(board)):
+        for j in range (9):
+            if board[i][j] == 0:
+                return [i,j]
+
+    return None
+
+print_board(board)
+solve(board)
+print("\n")
+print_board(board)
